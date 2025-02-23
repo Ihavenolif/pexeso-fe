4cc0feacc4eb8b2972fc79ba6ac7f8b8c89df2c6
ihavenolif <matuswall@gmail.com>
2025-02-23 19:07:24 +0100

    task update

--- src/pages/Game.svelte
+++ src/pages/Game.svelte
@@ -1,7 +1,7 @@
 <script>
     import { socket } from "../websocket";
     import { cards, lobbyInfo } from "../globals";
-    import { ClassColor } from "../util";
+    import { ClassColor, WowRole } from "../util";
     import FlipCard from "../components/FlipCard.svelte";
 
     /**
@@ -12,16 +12,56 @@
 
     let lobby;
     $: lobby = $lobbyInfo;
+
+    $: player1Tanks = lobby.clients[0].characters.filter(
+        (character) => character.wowRole === WowRole.TANK,
+    );
+    $: player1Healers = lobby.clients[0].characters.filter(
+        (character) => character.wowRole === WowRole.HEALER,
+    );
+    $: player1Dps = lobby.clients[0].characters.filter(
+        (character) => character.wowRole === WowRole.DPS,
+    );
+    $: player2Tanks = lobby.clients[1].characters.filter(
+        (character) => character.wowRole === WowRole.TANK,
+    );
+    $: player2Healers = lobby.clients[1].characters.filter(
+        (character) => character.wowRole === WowRole.HEALER,
+    );
+    $: player2Dps = lobby.clients[1].characters.filter(
+        (character) => character.wowRole === WowRole.DPS,
+    );
 </script>
 
 <main>
     <div class="column-container">
         <div class="column left">
-            {#each lobby.clients[0].characters as character}
-                <p style="color: {ClassColor[character.wowClass]}">
-                    {character.name}
-                </p>
-            {/each}
+            {#if player1Tanks.length > 0}
+                <h3>Tanks</h3>
+                {#each player1Tanks as character}
+                    <p style="color: {ClassColor[character.wowClass]}">
+                        {character.name}
+                    </p>
+                {/each}
+            {/if}
+
+            {#if player1Healers.length > 0}
+                <h3>Healers</h3>
+                {#each player1Healers as character}
+                    <p style="color: {ClassColor[character.wowClass]}">
+                        {character.name}
+                    </p>
+                {/each}
+            {/if}
+
+            {#if player1Dps.length > 0}
+                <h3>DPS</h3>
+                {#each player1Dps as character}
+                    <p style="color: {ClassColor[character.wowClass]}">
+                        {character.name}
+                    </p>
+                {/each}
+            {/if}
         </div>
         <div class="column middle">
             <div class="cards-container">
@@ -30,12 +70,33 @@
                 {/each}
             </div>
         </div>
-        <div class="column right">
-            {#each lobby.clients[1].characters as character}
-                <p style="color: {ClassColor[character.wowClass]}">
-                    {character.name}
-                </p>
-            {/each}
+        <div class="column right" style="text-align: right;">
+            {#if player2Tanks.length > 0}
+                <h3>Tanks</h3>
+                {#each player2Tanks as character}
+                    <p style="color: {ClassColor[character.wowClass]}">
+                        {character.name}
+                    </p>
+                {/each}
+            {/if}
+
+            {#if player2Healers.length > 0}
+                <h3>Healers</h3>
+                {#each player2Healers as character}
+                    <p style="color: {ClassColor[character.wowClass]}">
+                        {character.name}
+                    </p>
+                {/each}
+            {/if}
+
+            {#if player2Dps.length > 0}
+                <h3>DPS</h3>
+                {#each player2Dps as character}
+                    <p style="color: {ClassColor[character.wowClass]}">
+                        {character.name}
+                    </p>
+                {/each}
+            {/if}
         </div>
     </div>
 </main>
